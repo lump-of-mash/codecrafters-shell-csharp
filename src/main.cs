c79851993efaaf6f57f3679f0d14b756df9449d0
Dan <73538238+lump-of-mash@users.noreply.github.com>
2026-02-05 20:32:06 +0000

    Take input from users

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,8 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+        string? input = Console.ReadLine();
+        System.Console.WriteLine($"{input}: command not found");
     }
 }
