6143f7ac9806b0bf1798832fdb37b93bb2e7c0cc
Dan <73538238+lump-of-mash@users.noreply.github.com>
2026-02-06 19:12:04 +0000

    Added REPL

--- src/main.cs
+++ src/main.cs
@@ -2,10 +2,11 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
-
-        string? command = Console.ReadLine();
-        System.Console.WriteLine($"{command}: command not found");
+        while (true)
+        {
+            Console.Write("$ ");
+            string? command = Console.ReadLine();
+            System.Console.WriteLine($"{command}: command not found");
+        }
     }
 }
