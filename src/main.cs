5e0952f84c2229466589e754cb4d33903c0154a0
Dan <73538238+lump-of-mash@users.noreply.github.com>
2026-02-06 19:15:26 +0000

    Added exit command

--- src/main.cs
+++ src/main.cs
@@ -5,7 +5,14 @@ class Program
         while (true)
         {
             Console.Write("$ ");
+
             string? command = Console.ReadLine();
+
+            if(command == "exit")
+            {
+                break;
+            }
+
             System.Console.WriteLine($"{command}: command not found");
         }
     }
