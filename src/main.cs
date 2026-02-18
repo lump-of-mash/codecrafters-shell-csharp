0eb876ff5164891683e0e97b3b5d85c4ce6d93a2
Dan <73538238+lump-of-mash@users.noreply.github.com>
2026-02-18 19:46:14 +0000

    Adding input.clear() in hopes of tests passing

--- src/main.cs
+++ src/main.cs
@@ -89,13 +89,15 @@ class Program
             {
                 if (trie.Autocomplete(input.ToString(), out string suffix))
                 {
-                    var completeWord = input.ToString() + suffix;
-
+                    string completeWord = input.ToString() + suffix;
+                    
                     while(input.Length > 0)
                     {
                         Console.Write("\b \b");
                         input.Remove(input.Length - 1, 1);
-                    } 
+                    }
+                    input.Clear();
+                    
                     input.Append(completeWord);
                     Console.Write(completeWord);
                 }
